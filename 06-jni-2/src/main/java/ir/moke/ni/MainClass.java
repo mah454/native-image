f0cc17c6a96f1f1267cfee96c4bf037da7114701
mah454 <mah454@gmail.com>
2023-11-16 08:27:46 +0330

    06-jni-2

---
+++ 06-jni-2/src/main/java/ir/moke/ni/MainClass.java
@@ -0,0 +1,9 @@
+package ir.moke.ni;
+
+import ir.moke.jsysbox.system.JSystem;
+
+public class MainClass {
+    public static void main(String[] args) {
+        System.out.println(JSystem.getHostname());
+    }
+}
