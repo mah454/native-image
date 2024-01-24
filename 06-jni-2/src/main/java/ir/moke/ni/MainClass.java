1000437330c976762973c268e328accf47b7b60b
Mahdi Sheikh Hosseini <mah454@gmail.com>
2024-01-24 09:31:59 +0330

    CleanUp Project

--- 06-jni-2/src/main/java/ir/moke/ni/MainClass.java
+++ 06-jni-2/src/main/java/ir/moke/ni/MainClass.java
@@ -1,9 +1,9 @@
 package ir.moke.ni;
 
-import ir.moke.jsysbox.system.JSystem;
+import ir.moke.ni.jni.NativeLibrary;
 
 public class MainClass {
     public static void main(String[] args) {
-        System.out.println(JSystem.getHostname());
+        System.out.println(NativeLibrary.sum(12, 22));
     }
 }
